b186c8eb4e114caf96f209c17aee3fd4ae843bb7
Aiden Luce <luceaiden4124@gmail.com>
2023-06-30 13:36:14 -0500

    Person object

---
+++ src/main/java/Java_II/Person.java
@@ -0,0 +1,22 @@
+package Java_II;
+
+import java.util.Scanner;
+
+public class Person {
+
+    private String name;
+
+    public  String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void sayHello(){
+        System.out.printf("Hello %s!",name);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
