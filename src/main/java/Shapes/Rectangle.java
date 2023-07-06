5bf9bb4af9e422cb6ef45b91c2fd1d9177b07701
Aiden Luce <luceaiden4124@gmail.com>
2023-07-06 13:22:41 -0500

    Square extends Rectangle

---
+++ src/main/java/Shapes/Rectangle.java
@@ -0,0 +1,27 @@
+package Shapes;
+
+public class Rectangle {
+    protected double length;
+    protected double width;
+
+
+    public Rectangle (double length, double width){
+        this.length = length;
+        this.width = width;
+    }
+
+    public Rectangle() {
+
+    }
+
+    public double getArea(){
+        return (length * width);
+    }
+    public double getPerimeter(){
+        return (length + width *2);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/Shapes/shapesTest.java
@@ -0,0 +1,12 @@
+package Shapes;
+
+import Shapes.*;
+
+public class shapesTest {
+    static Rectangle box1 = new Rectangle(5,5);
+
+    public static void main(String[] args) {
+        System.out.println(box1.getArea());
+        System.out.println(box1.getPerimeter());
+    }
+}
